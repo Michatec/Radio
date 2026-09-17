a305caf37652bec456e698dad2897fa7676a1a17
Michachatz <121869403+Michatec@users.noreply.github.com>
2026-09-17 13:19:39 +0200

    Fix exclude path for LICENSE.base64.txt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,8 @@ android {
             excludes += "/META-INF/io.netty.versions.properties"
             excludes += "/META-INF/license/LICENSE.jbzip2.txt"
             excludes += "/META-INF/license/LICENSE.webbit.txt"
-            excludes += "META-INF/license/LICENSE.base64.txt"
+            excludes += "/META-INF/license/LICENSE.base64.txt"
+            excludes += "/META-INF/license/LICENSE.jzlib.txt"
         }
     }
 }
